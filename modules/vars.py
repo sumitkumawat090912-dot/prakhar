fd28e783a078451c0c6d7f9d6b8b854c6a930362
sumitkumawat090912-dot <sumitkumawat090912@gmail.com>
2025-11-19 17:48:02 +0530

    Update BOT_TOKEN in vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -5,7 +5,7 @@ from os import environ
 
 API_ID = int(environ.get("API_ID", "29899250"))
 API_HASH = environ.get("API_HASH", "611d045796c79af3e5ddfa3d6fd536a7")
-BOT_TOKEN = environ.get("BOT_TOKEN", "8461876221:AAHPI9rFngzd875t67t-54PLn_VXOaksHFw")
+BOT_TOKEN = environ.get("BOT_TOKEN", "8461876221:AAHOnNZamZlN7TnjN1pvgiFX2-CI4g6ug9I")
 
 OWNER = int(environ.get("OWNER", "7549194607"))
 CREDIT = environ.get("CREDIT", "@ll_kumawat_ll")
